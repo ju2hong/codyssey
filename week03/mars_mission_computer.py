20b2cc1100656d60c78e871c747dda4bcbf3fc09
ju2hong <juihong730@gmail.com>
2026-03-30 17:38:32 +0900

    feat: 보너스 과제

--- week03/mars_mission_computer.py
+++ week03/mars_mission_computer.py
@@ -1,4 +1,6 @@
 import random
+import time
+import os
 
 
 class DummySensor:
@@ -21,6 +23,23 @@ class DummySensor:
         self.env_values['mars_base_internal_oxygen'] = round(random.uniform(4, 7), 2)
 
     def get_env(self):
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        log_line = (
+            current_time +
+            ' | 화성 기지 내부 온도: ' + str(self.env_values['mars_base_internal_temperature']) +
+            ' | 화성 기지 외부 온도: ' + str(self.env_values['mars_base_external_temperature']) +
+            ' | 화성 기지 내부 습도: ' + str(self.env_values['mars_base_internal_humidity']) +
+            ' | 화성 기지 외부 광량: ' + str(self.env_values['mars_base_external_illuminance']) +
+            ' | 화성 기지 내부 이산화탄소 농도: ' + str(self.env_values['mars_base_internal_co2']) +
+            ' | 화성 기지 내부 산소 농도: ' + str(self.env_values['mars_base_internal_oxygen'])
+        )
+
+        file_path = os.path.join(os.path.dirname(__file__), 'mars_log.txt')
+
+        with open(file_path, 'a', encoding='utf-8') as f:
+            f.write(log_line + '\n')
+
         return self.env_values
 
 
