e681e0c2c05c5754f900b979b22562fc02067a2d
ju2hong <juihong730@gmail.com>
2026-04-06 15:46:37 +0900

    feat: 보너스 과제

--- week04/mars_mission_computer.py
+++ week04/mars_mission_computer.py
@@ -27,16 +27,21 @@ class JsonFormatter:
     @staticmethod
     def format(data):
         lines = ['{']
-        for key, value in data.items():
-            lines.append(f"  '{key}': {value},")
-        lines[-1] = lines[-1].rstrip(',')
+        keys = list(data.keys())
+
+        for i, key in enumerate(keys):
+            line = f'  "{key}": {data[key]}'
+            if i < len(keys) - 1:
+                line += ','
+            lines.append(line)
+
         lines.append('}')
         return '\n'.join(lines)
 
 
 class OutputHandler:
     @staticmethod
-    def print(data):
+    def display(data):
         formatted = JsonFormatter.format(data)
         print(formatted)
 
@@ -45,6 +50,9 @@ class MissionComputer:
     def __init__(self, sensor):
         self.sensor = sensor
         self.env_values = self._init_env_values()
+
+        self.history = []
+        self.max_history = 60  # 5초 * 60 = 5분
 
     def _init_env_values(self):
         return {
@@ -58,17 +66,48 @@ class MissionComputer:
 
     def update_env(self):
         sensor_data = self.sensor.read()
+        self.env_values.update(sensor_data)
+
+    def calculate_average(self):
+        avg = {}
+
         for key in self.env_values:
-            self.env_values[key] = sensor_data[key]
+            total = 0
+            for data in self.history:
+                total += data[key]
+            avg[key] = round(total / len(self.history), 3)
+
+        return avg
 
     def run(self):
         while True:
             self.update_env()
-            OutputHandler.print(self.env_values)
+
+            # 현재 상태 출력
+            OutputHandler.display(self.env_values)
+
+            # 데이터 저장
+            self.history.append(self.env_values.copy())
+
+            # 5분 평균 출력
+            if len(self.history) == self.max_history:
+                print("\n[5분 평균 값]")
+                avg = self.calculate_average()
+                OutputHandler.display(avg)
+                self.history.clear()
+
+            # 사용자 입력 (종료 여부 확인)
+            user_input = input("종료하려면 q 입력, 계속하려면 Enter: ")
+
+            if user_input.lower() == 'q':
+                print("System stopped...")
+                breakq
+                
+
             time.sleep(5)
 
 
 if __name__ == '__main__':
     ds = DummySensor()
-    RunComputer = MissionComputer(ds)
-    RunComputer.run()
+    mission_computer = MissionComputer(ds)
+    mission_computer.run()
